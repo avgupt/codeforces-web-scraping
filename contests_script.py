5cba6e9d7cf39e796588d63b228b699a86451579
avgupt <avishig012@gmail.com>
2020-05-07 18:34:55 +0530

    Check if row is empty

--- contests_script.py
+++ contests_script.py
@@ -15,13 +15,14 @@ contests_rows = contests_table_body.find_all('tr')
 for row in contests_rows:
     contests_cols = row.find_all('td')
     contests_cols = [ele.text.strip() for ele in contests_cols]
-    contests_data[int(contests_cols[0])] = {
-        'Contest' : contests_cols[1],
-        'Rank' : contests_cols[2],
-        'Solved' : contests_cols[3],
-        'Rating change' : contests_cols[4],
-        'New rating' : contests_cols[5]
-    }
+    if contests_cols:
+        contests_data[int(contests_cols[0])] = {
+            'Contest' : contests_cols[1],
+            'Rank' : contests_cols[2],
+            'Solved' : contests_cols[3],
+            'Rating change' : contests_cols[4],
+            'New rating' : contests_cols[5]
+        }
 
 with open('contests_data.txt', 'w') as contests_outfile:
     json.dump(contests_data, contests_outfile)
