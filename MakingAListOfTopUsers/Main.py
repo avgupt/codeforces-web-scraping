d8bc57ef1157f13c1f18592db7bf366a26e41369
ManviGoel26 <manvi19472@iiitd.ac.in>
2020-08-22 19:07:57 +0530

    Update Main.py

--- MakingAListOfTopUsers/Main.py
+++ MakingAListOfTopUsers/Main.py
@@ -4,18 +4,26 @@ only_active = "false"
 query = "https://codeforces.com/api/user.ratedList?activeOnly=" + only_active + "?lang=en"
 
 
-def listOfUsers():
+def listOfUsers(n):
 	with urllib.request.urlopen(query) as url:
 		data = json.loads(url.read().decode())
-	users = data["result"]
-	print(type(users))
-	return save_data(users)
+	users = data["result"][:n]
+
+	result = []
+	for i in users:
+		if 'country' in i:
+			result.append({'handle': i['handle'], 'country': i['country'], "maxRating": i['maxRating']})
+		else:
+			result.append({'handle': i['handle'], 'country': "Unknown", "maxRating": i['maxRating']})
+		
+		
+	return save_data(result)
 
 def save_data(user_list):
 	with open('user_list.txt', 'w') as outfile:
 		json.dump(user_list, outfile)
 
-listOfUsers()
+listOfUsers(10)
 
 
 	
