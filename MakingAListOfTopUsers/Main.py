bbd3bd4703657a0798933ec591330ab3416042c6
ManviGoel26 <manvi19472@iiitd.ac.in>
2020-08-22 13:12:18 +0530

    Update Main.py

--- MakingAListOfTopUsers/Main.py
+++ MakingAListOfTopUsers/Main.py
@@ -1,8 +1,14 @@
 import urllib.request, json
 
 only_active = "false"
-query = "https://codeforces.com/api/user.ratedList"
+query = "https://codeforces.com/api/user.ratedList" + only_active + "?lang=en"
 
 with urllib.request.urlopen(query) as url:
 	data = json.loads(url.read().decode())
-	print(data)
+print(data["result"][0].keys())
+#print(data["result"])
+#print(size(data["status"]))
+#for i in range(10):
+#	l = data["result"]
+#	print(l[i])
+	
