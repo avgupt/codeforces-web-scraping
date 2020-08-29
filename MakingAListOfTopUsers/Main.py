8b07c6eb4bd525ef43ece26fb27474f550266bc4
ManviGoel26 <manvi19472@iiitd.ac.in>
2020-08-30 00:01:28 +0530

    Update Main.py

--- MakingAListOfTopUsers/Main.py
+++ MakingAListOfTopUsers/Main.py
@@ -4,38 +4,40 @@ only_active = "false"
 query = "https://codeforces.com/api/user.ratedList?activeOnly=" + only_active + "?lang=en"
 
 
-def listOfUsers(n):
+def listOfUsers():
 
 	with urllib.request.urlopen(query) as url:
 		data = json.loads(url.read().decode())
 
-	users = data["result"][:n]
+	users = data["result"]
 
-	result = []
-	for i in users:
-		if 'country' in i:
-			result.append([i['handle'], i['country'], i['maxRating']])
+	result = {}
+	for i in range(len(users)):
+		if 'country' in users[i]:
+			result[i] = [users[i]['handle'], users[i]['country'], users[i]['maxRating']]
 		else:
-			result.append([ i['handle'], "Unknown", i['maxRating']])
+			result[i] = [ users[i]['handle'], "Unknown", users[i]['maxRating']]
 
-	result = sorted(result, key=lambda x: -x[2])
+
+
+	result = {k: v for k, v in sorted(result.items(), key=lambda item: -item[1][2])}
+
 
 	print_data(result)
 	return save_data(result)
 
 
 def save_data(user_list):
-	with open('user_list.txt', 'w') as outfile:
+	with open('user_list.json', 'w') as outfile:
 		json.dump(user_list, outfile)
 
 def print_data(user_list):
-	for i in user_list:
-		print( str(user_list.index(i)+1) + " " + i[0] + " " + i[1] + " " + str(i[2]))
+	for i in user_list.keys():
+		print( str(i+1) + " " + user_list[i][0] + " " + user_list[i][1] + " " + str(user_list[i][2]))
 
 
 if __name__ == "__main__":  
-	number = int(input(">"))
-	listOfUsers(number)
+	listOfUsers()
 
 
 
