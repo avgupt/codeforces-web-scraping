de5c038ab5f6eabad533dd5bed3e3798a6715fab
ManviGoel26 <manvi19472@iiitd.ac.in>
2020-10-02 17:23:45 +0530

    Update main.py

--- SavingUserSubmissions/main.py
+++ SavingUserSubmissions/main.py
@@ -3,32 +3,35 @@ from lxml import html
 from time import sleep
 
 # see start and end pages and specify in range
+# will throw an error at hidden submissions
+def returnAcceptedSubmissions(Username, number_of_pages):
 
-def returnAcceptedSubmissions(Username):
-
-    for foo in range(1,2):
+    for foo in range(1,number_of_pages):
+        sleep(10)
         # change address below accordingly
         page = requests.get('https://codeforces.com/submissions/' + Username + '/page/'+ str(foo))
         tree = html.fromstring(page.text)
     
         res = []
-    
+
+        checkStatus = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/a/@class')
         verdict = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[6]/span/@submissionverdict')
         contestID = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[4]/a/@href')
         problemID = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/a/text()')
-
+        #print(checkStatus)
         for i in range(len(contestID)):
             r = list(map(str,contestID[i].split("/")))
             #print(r)
             contestID[i] = r[2]
 
-        for i in range(len(contestID)):
-            res.append([contestID[i],verdict[i], problemID[i]])
-
-    
+        for i in range(len(problemID)):
+            res.append([contestID[i],verdict[i], problemID[i], checkStatus[i]])
+            #print(contestID[i],verdict[i], problemID[i], checkStatus[i])
+        #print(problemID)
+        
         for i in range(len(res)):
             # change verdict or dont specify at all
-            if res[i][1] == "OK":
+            if res[i][1] == "OK" and res[i][3] == "view-source":
                 solutionPage = requests.get('https://codeforces.com/contest/'+ res[i][0]+'/submission/'+ res[i][2])
                 print('https://codeforces.com/contest/'+ res[i][0]+'/submission/'+ res[i][2])
                 tree2 = html.fromstring(solutionPage.text)
@@ -44,4 +47,4 @@ def returnAcceptedSubmissions(Username):
                 sleep(5)
             
 
-returnAcceptedSubmissions("Um_nik")
+returnAcceptedSubmissions("Um_nik",5)
