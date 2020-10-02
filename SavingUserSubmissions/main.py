1e1fbaff90e2fcafbc0f45f1235ba491874eec11
ManviGoel26 <manvi19472@iiitd.ac.in>
2020-10-02 18:40:17 +0530

    Update main.py

--- SavingUserSubmissions/main.py
+++ SavingUserSubmissions/main.py
@@ -1,10 +1,10 @@
 import requests
 from lxml import html
-from time import sleep
+from time import sleep # to reduce the number of requests per second
 import json
 
 # see start and end pages and specify in range
-# will throw an error at hidden submissions
+
 def returnAcceptedSubmissions(Username, number_of_pages):
 
     for foo in range(1,number_of_pages):
@@ -15,11 +15,13 @@ def returnAcceptedSubmissions(Username, number_of_pages):
     
         res = []
 
+        #The data to obtain the URL of submissions
         checkStatus = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/*/@class')
         verdict = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[6]/span/@submissionverdict')
         contestID = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[4]/a/@href')
         problemID = tree.xpath('//*[@id="pageContent"]/div[4]/div[6]/table/tr[position() > 1]/td[1]/*/text()')
         
+        #Getting the contest ID
         for i in range(len(contestID)):
             r = list(map(str,contestID[i].split("/")))
             contestID[i] = r[2]
@@ -29,15 +31,21 @@ def returnAcceptedSubmissions(Username, number_of_pages):
            
 
         for i in range(len(res)):
-            # change verdict or dont specify at all
+            # change verdict or don't specify at all
+            # "view-source" neccessary as few submissions are not available(hidden-source)
             if res[i][1] == "OK" and res[i][3] == "view-source":
+                
                 solutionPage = requests.get('https://codeforces.com/contest/'+ res[i][0]+'/submission/'+ res[i][2])
                 tree2 = html.fromstring(solutionPage.text)
+                
+                # path to the actual submission
                 address = './/div[@id="pageContent"]/div[@class="roundbox SubmissionDetailsFrameRoundBox-'+res[i][2]+'"]/pre[@id="program-source-text"]//text()'
                 
                 code = tree2.xpath(address)
 
                 lines = code[0].split("\r\n")
+
+                #saving the code in a outfile
                 save_data(lines)
                 sleep(5)
             
@@ -50,6 +58,8 @@ def save_data(sub_data):
 
 
 
-Username = input("Enter the username: ")
-Number_of_pages = int(input("Enter the number of pages: "))
+Username = input("Enter the username: ") #Username
+Number_of_pages = int(input("Enter the number of pages: ")) #specify the total number of pages to inspect. Should be less than or equal to the total number.
+
+#running the program
 returnAcceptedSubmissions(Username,Number_of_pages)
